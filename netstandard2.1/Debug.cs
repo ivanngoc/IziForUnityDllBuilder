5d9e0a7d77ebc4b5c92b7cd44c95672133d05751
Tran Ngoc Anh <adycon94@gmail.com>
2024-11-04 00:15:55 +0300

    init

---
+++ netstandard2.1/Debug.cs
@@ -0,0 +1,17 @@
+﻿#if DEBUG
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IziHardGames.DllsForUnity
+{
+    internal class Debug
+    {
+        public void Test()
+        {
+            var json = System.Text.Json.JsonSerializer.Serialize(new object());
+        }
+    }
+}
+
+#endif
